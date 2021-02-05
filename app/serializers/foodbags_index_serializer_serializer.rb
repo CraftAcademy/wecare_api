c68629cf08095494318a4fc5e83590d7834233a2
flis_ann <ann.flismark@gmail.com>
2021-02-05 14:15:07 +0100

    Foodbag are displayed in descending order

--- app/serializers/foodbags_index_serializer_serializer.rb
+++ app/serializers/foodbags_index_serializer_serializer.rb
@@ -1,6 +1,7 @@
 class FoodbagsIndexSerializerSerializer < ActiveModel::Serializer
     attributes :id, :pickuptime, :status
     attribute :created_at do
+
     object.created_at.to_formatted_s(:db)
   end
 end
